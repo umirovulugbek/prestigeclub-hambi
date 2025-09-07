08f4205a8a1ba62234e62c576e72667b479791d3
Ulug'bek Umirov <umirovulugbek3@gmail.com>
2025-09-07 22:51:11 +0500

    fix: improve LastMinuteTours component to conditionally render content based on data availability

--- src/components/main/LastMinuteTours.jsx
+++ src/components/main/LastMinuteTours.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import FireIcon from '../../icons/FireIcon';
 import Axios from '../../utils/httpsClinet';
 import LastMinuteTourHotel from '../card/LastMinuteTourHotel';
-import { useTranslation } from 'react-i18next';
 
 const LastMinuteTours = () => {
 	const [data, setData] = useState([]);
-	const { t } = useTranslation()
+	const { t } = useTranslation();
 	useEffect(() => {
 		getHotTour();
 	}, []);
@@ -21,21 +21,25 @@ const LastMinuteTours = () => {
 			});
 	};
 	return (
-		<div className='pt-[20px] container_main '>
-			<div className='text-[20px] dark:text-white flex gap-[5px] items-center mb-4'>
-				<FireIcon />
-				{t('home.hotTours')}
-			</div>
-			<div className='grid grid-cols-2 gap-3'>
-				{data?.data?.map((item, index) => {
-					return (
-						<React.Fragment key={index}>
-							<LastMinuteTourHotel data={item} />
-						</React.Fragment>
-					);
-				})}
-			</div>
-		</div>
+		<>
+			{data?.data?.length > 0 ? (
+				<div className='pt-[20px] container_main '>
+					<div className='text-[20px] dark:text-white flex gap-[5px] items-center mb-4'>
+						<FireIcon />
+						{t('home.hotTours')}
+					</div>
+					<div className='grid grid-cols-2 gap-3'>
+						{data?.data?.map((item, index) => {
+							return (
+								<React.Fragment key={index}>
+									<LastMinuteTourHotel data={item} />
+								</React.Fragment>
+							);
+						})}
+					</div>
+				</div>
+			) : null}
+		</>
 	);
 };
 
